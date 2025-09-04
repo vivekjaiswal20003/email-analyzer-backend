f8d2b35839ab728275834f2b89a62cc73c1d33cc
vivekjaiswal20003 <vickyguddu12@gmail.com>
2025-09-04 11:06:13 +0530

    Update configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   port: parseInt(process.env.PORT || '5000', 10),
   database: {
-    uri: process.env.MONGO_URI || 'mongodb+srv://new-user:root@cluster0.qyr92tu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    uri: process.env.MONGO_URI || 'mongodb-url',
   },
 });
